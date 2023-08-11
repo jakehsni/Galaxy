5f27a5e3c2bbb4db51e7a0c73b859f5755663f7d
SkyConnor <kamalhoseini68@gmail.com>
2023-08-11 18:56:11 +0330

    seperate buffer from storage

--- Tensordef.rs
+++ Tensordef.rs
@@ -5,17 +5,16 @@ use std::mem;
 mod buffer;
 mod shape;
 mod utility;
-use buffer::Storage;
+use buffer::Buffer;
 
 #[derive(Debug)]
 struct Tensor{
-    storage : Storage,
+    data : Buffer,
     strides : Vec<usize>,
     dimensions :  Vec<usize>,
     viewcnt : usize,
     orginal: bool,
     ndim : usize,
-    size : usize
 }
 
 
@@ -25,10 +24,127 @@ impl Tensor{
 fn data(&self) -> *mut u8 {
     self.storage.data_ptr()
 }
+//=======add
+fn add_tensor(&self, other : &Tensor) -> Self {
+if (self.size != other.size){
+    println
+}
+let ptr = self.data();
+let ptr_other = other.data();
 
+for i in 0..slef.dimensions{
+   *ptr_out.add(i) (*ptr.add(i))  + (*ptr_other.add(i));
 
+}
+Self{strides : self.strides.to_vec(),
+    dimensions : self.dimensions.to_vec(),
+    size : self.size,
+    ndim : self.ndim,
 
+}
 
+}
+
+//=======add_inplace
+fn add_inplace_tensor(&mut self, other : &Tensor) -> Self {
+    if (self.size != other.size){
+        println
+    }
+    let ptr = self.data();
+    let ptr_other = other.data();
+    
+    for i in 0..slef.dimensions{
+       *ptr.add(i) = (*ptr.add(i))  + (*ptr_other.add(i));
+    
+    }
+   
+    
+    }
+    
+
+//=========sub
+fn sub_tensor(&self, other : &Tensor) -> Self {
+    if (self.size != other.size){
+        println
+    }
+    let ptr = self.data();
+    let ptr_other = other.data();
+    
+    for i in 0..slef.dimensions{
+       *ptr_out.add(i)  = (*ptr.add(i))  - (*ptr_other.add(i));
+    
+    }
+    Self{strides : self.strides.to_vec(),
+        dimensions : self.dimensions.to_vec(),
+        size : self.size,
+        ndim : self.ndim,
+    
+    }
+    
+}
+
+//=========sub_inplace
+fn sub_inplace_tensor(&mut self, other : &Tensor) -> Self {
+    if (self.size != other.size){
+        println
+    }
+    let ptr = self.data();
+    let ptr_other = other.data();
+    
+    for i in 0..slef.dimensions{
+       *ptr.add(i)  = (*ptr.add(i))  - (*ptr_other.add(i));
+    
+    }
+    Self{strides : self.strides.to_vec(),
+        dimensions : self.dimensions.to_vec(),
+        size : self.size,
+        ndim : self.ndim,
+    
+    }
+    
+}
+//=======mul
+fn mul_tensor(&self, other : &Tensor) -> Self {
+    if (self.size != other.size){
+        println
+    }
+    let ptr = self.data();
+    let ptr_other = other.data();
+    
+    for i in 0..slef.dimensions{
+       *ptr_out.add(i)  = (*ptr.add(i))  * (*ptr_other.add(i));
+    
+    }
+    Self{strides : self.strides.to_vec(),
+        dimensions : self.dimensions.to_vec(),
+        size : self.size,
+        ndim : self.ndim,
+    
+    }
+    
+}
+
+//=======mul_inplace
+fn mul_inplace_tensor(&mut self, other : &Tensor) -> Self {
+    if (self.size != other.size){
+        println
+    }
+    let ptr = self.data();
+    let ptr_other = other.data();
+    
+    for i in 0..slef.dimensions{
+       *ptr.add(i) =  (*ptr.add(i))  * (*ptr_other.add(i));
+    
+    }
+    Self{strides : self.strides.to_vec(),
+        dimensions : self.dimensions.to_vec(),
+        size : self.size,
+        ndim : self.ndim,
+    
+    }
+    
+}
+    
 
 
 
--- buffer.rs
+++ buffer.rs
@@ -2,127 +2,65 @@ use std::ptr:: NonNull;
 use std::alloc::{self, Layout};
 use std::mem;
 
-
+mod storage;
+use storage::Storage;
 
 
 
 #[derive(Debug)]
- pub struct Storage {
+ pub struct Buffer {
 
-    data: *mut u8,  
-    cap_bytes: usize 
+    buf: Storage,
+    len : usize
    
 }
 
 
 impl<T> Buffer<T>{
 
-    pub const fn new() -> Self {
-        Self { ptr: NonNull::<T>::dangling(), cap_bytes: 0 }
+    pub fn replace_from_vector(&mut self, vector: &Vec<>) {
+    let len = vector.len();
+    if (len >= self.buf.capacity ){
+        self.buf.grow_exact(len)
     }
+    self.buf.copy_from(vector.as_ptr(), len);
 
-    pub fn with_capacity(capacity: usize) -> Self {
-        Self::allocate(capacity)
-    }
-
-    pub unsafe fn from_raw_parts(ptr: *mut T, capacity: usize) -> Self {
-        Self { ptr: unsafe { NonNull::new_unchecked(ptr) }, cap_bytes: capacity }
-    }
-
-
-    pub fn copy_from(&self, source : *const T, size: usize){
-        let dest_pointer = self.ptr.as_ptr()
-        unsafe {std::ptr::copy(source, dest_pointer, size)};
-
-
-
-
-    }
-  
-    pub fn copy(&self) -> Self{
-        
-        let layout = Layout::array::<T>(self.cap_bytes).unwrap();
-        let result = unsafe{alloc::alloc(layout) };
-
-        let copy_inst = Storage {
-                ptr: unsafe { NonNull::new_unchecked(result as *mut T) },
-                cap_bytes: self.capacity,
-            }
-
-        }
-
-
-
-    }
-    fn allocate(capacity: usize) -> Self {
-      
-        let layout = Layout::array::<T>(capacity).unwrap();
-        let result = unsafe{alloc::alloc(layout) };
-            
-        Self {
-                ptr: unsafe { NonNull::new_unchecked(result as *mut T) },
-                cap_bytes: capacity,
-            }
-        }
-    
-    pub fn grow_exact(&mut self, unit: usize) {
-        self.cap_bytes += unit;
-        let new_layout = Layout::array::<T>(self.cap_bytes).unwrap();
-        let new_ptr =  unsafe{alloc::alloc(new_layout)};
-            
-        self.ptr = match NonNull::new(new_ptr as *mut T) {
-                                    Some(p) => p,
-                                    None => alloc::handle_alloc_error(new_layout),
-                                };  
-
-    }
-
-  
-    pub fn grow_mul(&mut self, mul: usize) {
-        self.cap_bytes *= mul;
-        let new_layout = Layout::array::<T>(self.cap_bytes).unwrap();
-        let new_ptr = if self.cap_bytes==0 {
-            unsafe{alloc::alloc(new_layout)}
-                                    } else {unsafe{alloc::alloc(new_layout)}};
-                
-        self.ptr = match NonNull::new(new_ptr as *mut T) {
-                                        Some(p) => p,
-                                        None => alloc::handle_alloc_error(new_layout),
-                                    };  
-    
+    self.len = len;
     }
 
 
-    pub fn grow(&mut self){
-        let (new_cap, new_layout) = if self.cap_bytes ==0{ (1, Layout::array::<T>(1).unwrap())} else {
-            let new_c = 2 * self.cap_bytes;
-            let new_l = Layout::array::<T>(new_c).unwrap();
-            (new_c, new_l)
-
-        };
-        
-        let new_ptr = if self.cap_bytes==0 {
-            unsafe{alloc::alloc(new_layout)}
-                                    } else {unsafe{alloc::alloc(new_layout)}};
-                
-        self.ptr = match NonNull::new(new_ptr as *mut T) {
-                                        Some(p) => p,
-                                        None => alloc::handle_alloc_error(new_layout),
-                                    };                           
-        self.cap_bytes = new_cap;
+    pub fn take_vector(&mut self, vector: &Vec<>) {
+    self.len = vector.len();
+    self.buf.from_raw_parts(vector.as_ptr(), self.len);
     }
 
+    pub fn replace_from_slice(&mut self, slice: &[T]) {
+        self.len = slice.len();
+        self.buf.copy_from(slice.as_ptr(), self.len);
+    }
 
+    pub fn copy_to(&self) -> Self {
+        let buf  = self.buf.copy_to();
+        let len = self.len();
+        std::ptr::copy(self.data_ptr(), buf.data_ptr(), len);
+        Self{buf: buf,
+        len : len}
+    }
+
+    pub fn release(&mut self) {
+        self.len = 0;
+    }
     
 
-    pub fn capacity(&self) -> usize {
-         self.cap_bytes 
+
+
+    pub fn len(&self) -> usize {
+        self.len
     }
 
-    pub fn data_ptr(&self) -> *mut u8 {
-        self.data
+    pub fn data_ptr(&self) -> *mut T {
+        self.buf.data_ptr()
     }
-
   
 }
 
